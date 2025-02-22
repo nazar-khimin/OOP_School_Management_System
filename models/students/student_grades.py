f88f16276cc4a5c0b4976287ab83f0b43def521a
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 15:05:32 +0200

    - added fields to student_courses.py

---
+++ models/students/student_grades.py
@@ -0,0 +1,14 @@
+from scipy.stats import genpareto_gen
+from sqlalchemy import Uuid, String, Integer, ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+from models.timestamp_mixin import TimestampMixin
+
+@genpareto_gen
+class Students(Base, TimestampMixin):
+    course_id: Mapped[str] = mapped_column(Uuid, ForeignKey('courses.id'))
+    students_id: Mapped[str] = mapped_column(Uuid, ForeignKey('students.id'))
+    teachers_id: Mapped[str] = mapped_column(Uuid, ForeignKey('teachers.id'))
+    school_id: Mapped[str] = mapped_column(Uuid, ForeignKey('schools.id'))    #
+    grade: Mapped[int] = mapped_column(Integer)
