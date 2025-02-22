d05a7c800941e83320fa38bca0d64bb4eda20a79
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 15:22:02 +0200

    - added fields to teacher and teacher courses

---
+++ models/teachers/teacher_courses.py
@@ -0,0 +1,12 @@
+from scipy.stats import genpareto_gen
+from sqlalchemy import Uuid, String, Integer, ForeignKey, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+from models.timestamp_mixin import TimestampMixin
+
+@genpareto_gen
+class Teachers(Base, TimestampMixin):
+    __tablename__ = "teacher_courses"
+    teacher_id: Mapped[str] = mapped_column(Uuid, ForeignKey('teachers.id'))
+    course_id: Mapped[str] = mapped_column(Uuid, ForeignKey('courses.id'))
---
+++ models/teachers/teachers.py
@@ -0,0 +1,14 @@
+from scipy.stats import genpareto_gen
+from sqlalchemy import Uuid, String, Integer, ForeignKey, ARRAY
+from sqlalchemy.orm import Mapped, mapped_column
+
+from models.base import Base
+from models.timestamp_mixin import TimestampMixin
+
+@genpareto_gen
+class Teachers(Base, TimestampMixin):
+    __tablename__ = "teachers"
+    name: Mapped[str] = mapped_column(String(30))
+    subject_specialties: Mapped[list] = mapped_column(ARRAY(String))
+    grade_level: Mapped[int] = mapped_column(Integer)
+    school_id: Mapped[str] = mapped_column(Uuid, ForeignKey('schools.id'))
