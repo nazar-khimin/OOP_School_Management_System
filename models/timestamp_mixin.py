cbf24bc593573948e2418c6fd425391f6df6bc64
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 13:50:52 +0200

    - added timestamp mix-in using SQLAlchemy 2.0 version
    ref:
    https://docs.sqlalchemy.org/en/20/orm/declarative_mixins.html

--- models/timestamp_mixin.py
+++ models/timestamp_mixin.py
@@ -5,4 +5,4 @@ from sqlalchemy.orm import Mapped, mapped_column
 
 class TimestampMixin:
     created_at: Mapped[datetime] = mapped_column(default=func.now())
-    updated_at: Mapped[datetime]
+    updated_at: Mapped[datetime] = mapped_column(default=func.now(), onupdate=func.now())
