f608d932489f6a5c81851b8cb757e20a6abfda0f
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 14:27:47 +0200

    - added all fields to course.py

--- models/course.py
+++ models/course.py
@@ -1,6 +1,6 @@
 from typing import Optional
 
-from sqlalchemy import String
+from sqlalchemy import String, Uuid, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column
 from models.base import Base
 from models.timestamp_mixin import TimestampMixin
@@ -11,4 +11,7 @@ class Course(Base, TimestampMixin):
     __tablename__ = 'courses'
     id: Mapped[int] = mapped_column(primary_key=True)
     name: Mapped[str] = mapped_column(String(30))
-    description: Mapped[Optional[str]] = mapped_column(String(200))
+    teacher_id: Mapped[str] = mapped_column(Uuid, ForeignKey('teachers.id'))
+    max_capacity: Mapped[int] = mapped_column(Integer)
+    required_grade_level: Mapped[int] = mapped_column(Integer)
+    credits: Mapped[int] = mapped_column(Integer)
