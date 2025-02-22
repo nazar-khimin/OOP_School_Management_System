3466fa88477f9f281aa0bd2a7a0ebe654017a753
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 16:21:06 +0200

    remove @declarative_mixin from Base class

--- models/base.py
+++ models/base.py
@@ -1,9 +1,8 @@
 import uuid
 
 from sqlalchemy import UUID
-from sqlalchemy.orm import DeclarativeBase, declarative_mixin, Mapped, mapped_column
+from sqlalchemy.orm import DeclarativeBase, Mapped, mapped_column
 
-@declarative_mixin
 class Base(DeclarativeBase):
     __abstract__ = True
     id: Mapped[uuid.UUID] = mapped_column(
