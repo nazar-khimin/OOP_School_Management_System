2cbff8a0782df25bc862c267e80b94975b6b566f
nazar.khimin <nazar.khimin@unanet.com>
2025-02-22 15:42:45 +0200

    renamed file to singularity form

--- models/schools/school_course.py
+++ models/schools/school_course.py
@@ -1,13 +1,14 @@
-from sqlalchemy import Uuid, String, ForeignKey
+from sqlalchemy import Uuid, ForeignKey
 from sqlalchemy.orm import Mapped, relationship
 from sqlalchemy.testing.schema import mapped_column
 
 from models.base import Base
+from models.schools.school import Schools
 from models.timestamp_mixin import TimestampMixin
 from repr.repr_generator import generate_repr
 
 @generate_repr()
-class SchoolCourses(Base, TimestampMixin):
+class SchoolCourse(Base, TimestampMixin):
     __tablename__ = "school_courses"
     school_id: Mapped[str] = mapped_column(Uuid, ForeignKey('schools.id'))
     course_id: Mapped[str] = mapped_column(Uuid, ForeignKey('courses.id'))
